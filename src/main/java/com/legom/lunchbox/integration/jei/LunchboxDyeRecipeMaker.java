4c16b04c3d8b40fa1bf3a52ad5f7f34e5b5c0cbf
LegoMaster3650 <37251737+LegoMaster3650@users.noreply.github.com>
2023-02-19 17:54:16 -0600

    Removed invalid lunchbox dye recipes from JEI

--- src/main/java/com/legom/lunchbox/integration/jei/LunchboxDyeRecipeMaker.java
+++ src/main/java/com/legom/lunchbox/integration/jei/LunchboxDyeRecipeMaker.java
@@ -6,12 +6,9 @@ import java.util.stream.Stream;
 
 import com.legom.lunchbox.Lunchbox;
 import com.legom.lunchbox.items.LunchboxItem;
-import com.legom.lunchbox.registry.ModItemTags;
-
 import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.DyeColor;
-import net.minecraft.world.item.DyeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingRecipe;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -22,16 +19,17 @@ public class LunchboxDyeRecipeMaker {
 	private static final String group = "lunchbox.color";
 	
 	public static List<CraftingRecipe> getRecipes() {
-		Ingredient lunchboxBase = Ingredient.of(ModItemTags.LUNCHBOX);
 		return Arrays.stream(DyeColor.values())
-				.map(color -> createRecipe(color, lunchboxBase))
+				.map(color -> createRecipe(color))
 				.toList();
 	}
 	
-	private static CraftingRecipe createRecipe(DyeColor color, Ingredient baseLunchbox) {
-		Ingredient colorIn = Ingredient.fromValues(Stream.of(new Ingredient.ItemValue(new ItemStack(DyeItem.byColor(color))), new Ingredient.TagValue(color.getTag())));
+	private static CraftingRecipe createRecipe(DyeColor color) {
+		Ingredient lunchboxes = Ingredient.of(Stream.concat(Stream.of(LunchboxItem.byColor(null)), Arrays.stream(DyeColor.values()).filter(col -> col != color).map(col -> LunchboxItem.byColor(col))).map(ItemStack::new));
+//		Ingredient colorIn = Ingredient.fromValues(Stream.of(new Ingredient.ItemValue(new ItemStack(DyeItem.byColor(color))), new Ingredient.TagValue(color.getTag())));
+		Ingredient colorIn = Ingredient.of(color.getTag());
 		
-		NonNullList<Ingredient> inputs = NonNullList.of(Ingredient.EMPTY, baseLunchbox, colorIn);
+		NonNullList<Ingredient> inputs = NonNullList.of(Ingredient.EMPTY, lunchboxes, colorIn);
 		
 		ItemStack output = new ItemStack(LunchboxItem.byColor(color));
 		ResourceLocation id = new ResourceLocation(Lunchbox.MOD_ID, group + "." + output.getDescriptionId());
